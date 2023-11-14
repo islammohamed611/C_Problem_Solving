77feeed4a04b77305ed7b152129d216bc2d54730
islammohamed611 <89746353+islammohamed611@users.noreply.github.com>
2023-11-14 22:58:04 +0200

    Problem Solving

---
+++ 1_C_Basics/1-Swap_Two_Num_without_Temp/Solution1.c
@@ -0,0 +1,34 @@
+/******************************************************************************************************************************************
+ Assignment  : Swap_Two_Numbers_Without_Temp_Sol1.c
+ Author      : Islam Mohamed Abdullah
+ Date  		 : Nov  14 - 2023
+
+******************************************************************************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+void main ()
+{
+	
+	float  Num1 , Num2 ;
+	
+	printf ("Enter value of Num1 :");
+	scanf  ("%f" , &Num1 );
+	printf ("Enter value of Num2 :");
+	scanf  ("%f" , &Num2 );
+	printf ("\n");
+	
+	
+	Num1 = Num1  ^  Num2 ;
+	Num2 = Num1  ^  Num2 ;
+	Num1 = Num1  ^  Num2 ;
+	
+	printf ("After swappin , the value of Num1 = %f\n",Num1);
+	printf ("After swappin , the value of Num2 = %f\n",Num2);
+	
+	
+	
+	
+	
+	
+	
+}
