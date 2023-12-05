0dad3b8ac0e15522bc6f42cf0c370a09177ec124
islammohamed611 <89746353+islammohamed611@users.noreply.github.com>
2023-12-05 20:53:01 +0200

    6-store_info_of_students

---
+++ 6-Structure_union_enum/6-store_info_of_students/main.c
@@ -0,0 +1,34 @@
+/***************************************************************************
+ Assignment  : Store_Info_of_Student_Using_Struct.c
+ Author      :Islam Mohamed Abdullah
+ Date  		 : DEC  5 - 2022
+****************************************************************************/
+
+#include <stdio.h>
+
+struct S_Info 
+{
+	char Name[50];
+	int Roll;
+	float Marks;
+}S_Student;
+
+void main ()
+{
+	printf ("\nEnter Information of the student : \n");
+	
+	printf ("Enter Name: ");
+	gets (S_Student.Name);
+
+	printf ("Enter Roll Number: ");
+	scanf ("%d",&S_Student.Roll);
+
+	printf ("Enter Marks: ");
+	scanf ("%f",&S_Student.Marks);
+	
+	printf ("\nDisplay Info :- \n");
+	printf ("Name: %s\n",S_Student.Name);
+	printf ("Roll: %d\n",S_Student.Roll);
+	printf ("Marks: %.1f\n",S_Student.Marks);
+
+}
