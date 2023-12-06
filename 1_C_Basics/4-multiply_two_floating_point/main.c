310ec7a1a3eb25bc9fe265ea1b100f0f336baa4b
islammohamed611 <89746353+islammohamed611@users.noreply.github.com>
2023-12-06 19:37:29 +0200

    4-multiply_two_floating_point

---
+++ 1_C_Basics/4-multiply_two_floating_point/main.c
@@ -0,0 +1,17 @@
+/***************************************************************************
+ Assignment  : Multiply_two_Floating_Point_Numbers.c
+ Author      : Islam Mohamed Abdullah 
+ Date  		 : DEC  6 - 2022
+****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main ()
+{
+	float Num1 ,Num2 ;                                
+	printf ("Enter two integers: ");        
+	scanf  ("%f", &Num1 );                     
+	scanf  ("%f", &Num2 );                     
+	printf ("Product: %f\n", Num1*Num2 );     
+}
