6b291c8952099731f19807daf70b09785db85758
islammohamed611 <89746353+islammohamed611@users.noreply.github.com>
2023-12-06 19:38:39 +0200

    6-print_sentence

---
+++ 1_C_Basics/6-print_sentence/main.c
@@ -0,0 +1,11 @@
+/***************************************************************************
+ Assignment  : EX1_C_Program_To_Print_Sentence.c
+ Author      : Islam Mohamed Abdullah 
+ Date  		 : DEC  6 - 2022
+****************************************************************************/
+
+#include <stdio.h>
+void main ()
+{
+	printf ("C Programming\n");
+}
