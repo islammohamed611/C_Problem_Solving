fb9296eeed8e6e2c5d211dcf100a5d9aebe87bff
islammohamed611 <89746353+islammohamed611@users.noreply.github.com>
2023-12-08 22:04:31 +0200

    3-find_sum_of_natural_nums

---
+++ 2-Condition_Loops/3-find_sum_of_natural_nums/main.c
@@ -0,0 +1,15 @@
+/**********************************************************************************
+ Assignment  : C_Program_to_calculate_sum_of_natural_numbers.c
+ Author      :  Islam Mohamed Abdullahl
+ Date  		 :DEC  8 - 2022
+***********************************************************************************/
+
+#include <stdio.h>
+void main ()
+{
+	int i ;
+	printf ("Enter an integer: ");
+	scanf ("%i",&i);
+	printf ("Sum = %i",(i*(i+1)/2));
+
+}
