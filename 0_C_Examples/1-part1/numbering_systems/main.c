cac592f04f7bbf8e8e5791385d51c15d58406f29
islammohamed611 <89746353+islammohamed611@users.noreply.github.com>
2023-12-18 23:16:29 +0200

    added new c examples

---
+++ 0_C_Examples/1-part1/numbering_systems/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main ()
+{
+	int a,b,c,d ;
+	a=50;
+	b=0x32;
+	c=062;
+	d=0b110010;
+	
+	printf("%p\n",a);
+	printf("%d\n",b);
+	printf("%d\n",c);
+	printf("%d\n",d);
+	return 0;
+}
+
