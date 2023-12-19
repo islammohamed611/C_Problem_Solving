7dada2c9b37ccf7d1f48b4172640179885dc21d5
islammohamed611 <89746353+islammohamed611@users.noreply.github.com>
2023-12-19 18:28:53 +0200

    added new code

---
+++ 0_C_Examples/1-part1/Asci_char/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main ()
+{
+	char x='97';
+	char y=97;
+	
+	printf("%i\n",x); //55
+	printf("%c\n",x); //7
+	
+	printf("%i\n",y); //97
+	printf("%c\n",y); //a
+	
+	return 0;
+}
+
