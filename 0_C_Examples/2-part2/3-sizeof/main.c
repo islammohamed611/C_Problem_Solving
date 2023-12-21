edc558d8ae0712b581ca84172fdd4cb576af4545
islammohamed611 <89746353+islammohamed611@users.noreply.github.com>
2023-12-21 23:27:25 +0200

    added new code

---
+++ 0_C_Examples/2-part2/3-sizeof/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+int main ()
+{
+	char a ; // 1 byte
+   short b ; // 2byte
+   int c ; //4 byte
+   long d ; //4 byte
+   long long e; //8 byte
+   float f; //4 byte
+   double g ; //8 byte
+   long double h ; //12 byte
+   printf("size of char=%i\n",sizeof(a));
+   printf("size of short=%i\n",sizeof(b));
+   printf("size of int=%i\n",sizeof(c));
+   printf("size of long=%i\n",sizeof(d));
+   printf("size of long long =%i\n",sizeof(e));
+   printf("size of float=%i\n",sizeof(f));
+   printf("size of double=%i\n",sizeof(g));
+   printf("size of long double=%i\n",sizeof(h));
+   
+   /* Tricky Code */
+	
+	int  k=15 ,x=10 ,y ;
+	y=sizeof(k=x+2);
+	printf("%d\n",k);
+	printf("%d\n",y);
+	
+	return 0;
+}
+
