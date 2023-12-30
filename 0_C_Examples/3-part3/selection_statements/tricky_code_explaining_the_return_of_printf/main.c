a8d954f03409a75190e7b69d59e03fae6471ae40
islammohamed611 <89746353+islammohamed611@users.noreply.github.com>
2023-12-30 19:23:59 +0200

    added new  code

---
+++ 0_C_Examples/3-part3/selection_statements/tricky_code_explaining_the_return_of_printf/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int y,z;
+    y=printf("Hello world!\n"); //returns the number of characters
+    z=printf("hello");
+    printf("\n");
+    printf("y=%i\nz=%i",y,z);
+    return 0;
+}
