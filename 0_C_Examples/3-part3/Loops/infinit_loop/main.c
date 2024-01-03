ec4c2b87d417ec6dd1978153b16aacf6ba240a70
islammohamed611 <89746353+islammohamed611@users.noreply.github.com>
2024-01-03 19:27:29 +0200

    added new code

---
+++ 0_C_Examples/3-part3/Loops/infinit_loop/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+int i;
+for(;  ;)
+    {
+        printf("hello world\n");
+    }
+/*
+     while(1)
+    {
+       printf("hello world\n");
+    }
+*/
+     return 0;
+}
+
+
+
+
