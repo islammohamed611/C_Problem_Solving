ad92e310540024ffcdfe6262c67ed1325a908845
islammohamed611 <89746353+islammohamed611@users.noreply.github.com>
2024-01-21 23:55:57 +0200

    added new code

---
+++ 0_C_Examples/4-part4/print_octal_value/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    x=50;
+    printf("x=%i\n",x);
+    printf("x=%o\n",x);
+    printf("x=%p\n",x);
+    printf("x=%x\n",x);
+    printf("\n\n");
+    x=062;
+    printf("x=%i\n",x);
+    printf("x=%o\n",x);
+    printf("x=%p\n",x);
+    printf("x=%x\n",x);
+    return 0;
+}
