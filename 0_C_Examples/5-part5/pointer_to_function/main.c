c1268be294cc0fb621b1326b75f8df4739f336a4
islammohamed611 <89746353+islammohamed611@users.noreply.github.com>
2024-02-01 20:12:45 +0200

    added new code

---
+++ 0_C_Examples/5-part5/pointer_to_function/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int sum(int x,int y);
+int main()
+{
+    int result;
+    int (*ptr)(int,int);
+    ptr=sum;
+    result=ptr(10,20);
+    printf("sum=%i",result);
+    return 0;
+}
+int sum(int x,int y)
+{
+    return(x+y);
+}
